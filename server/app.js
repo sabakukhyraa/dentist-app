7393b551db877a5ad4bb8162c220322f4c126c46
Ali Kerem Ata <96057812+sabakukhyraa@users.noreply.github.com>
2024-02-08 16:14:16 +0300

    file name fixing and defining the dbName

--- server/app.js
+++ server/app.js
@@ -1,23 +1,30 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 const express = require("express");
-const Patient = require("./models/patient.js");
+const Patient = require("./models/patientModel.js");
 const patientRoutes = require("./routes/patients.js");
 
 // express app
 const app = express();
 
 // middleware
+app.use(express.json());
+
 app.use((req, res, next) => {
-  console.log(req.path, req.method);
+  console.log(
+    `------------------------\nThe path the request came from: ${req.path}\nRequest method: ${req.method}\n------------------------`
+  );
   next();
 });
 
 //routes
 app.use("/api/patients", patientRoutes);
 
+// connect to db
 mongoose
-  .connect(process.env.DB_URL)
+  .connect(process.env.DB_URL, {
+    dbName: "DentistApp",
+  })
   .then((result) => {
     app.listen(process.env.PORT, () => {
       console.log(`Server is running on port ${process.env.PORT}`);
---
+++ server/models/patientModel.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// There is an one-to-many relationship between Patients and DefinedTeeth.
+// Patients are the "one" side of the relationship and DefinedTeeth are the "many" side.
+const toothSchema = new Schema({
+  toothNumber: {
+    type: Number,
+    required: true,
+  },
+  // Can create Treatment Schema for this particular property as an array [treatmentSchema] later.
+  treatmentsBefore: {
+    type: Array,
+    required: false,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+const patientSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    birthDate: {
+      type: Date,
+      required: true,
+    },
+    definedTeeth: [toothSchema],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Patient", patientSchema);
