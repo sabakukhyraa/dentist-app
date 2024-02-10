1cf11c8ff1d516ff72566c63dd4858736bf98a83
Ali Kerem Ata <96057812+sabakukhyraa@users.noreply.github.com>
2024-02-10 12:40:24 +0300

    simple design

--- client/src/components/DefineTooth.jsx
+++ client/src/components/DefineTooth.jsx
@@ -1,37 +1,51 @@
-export default function DefineTooth({
-  toothNumber,
-  toothDescription,
-  toothTreatmentsBefore,
-}) {
+export default function DefineTooth({ toothNumber, toothDescription, toothTreatmentsBefore, setIsModalOpen, }) {
+
+
   const treatments = toothTreatmentsBefore.map((treat, index) => {
     return (
-      <div key={index}>
-        <label htmlFor={`treatment-name-${index}`}>Treatment {index}</label>
-        <input
-          type="text"
-          name={`treatment-name-${index}`}
-          id={`treatment-name-${index}`}
-          value={treat}
-        />
-        <button>Remove</button>
-      </div>
+      <li className="flex justify-between border p-1" key={index}>
+        <div>
+          <label className="mr-4" htmlFor={`treatment-name-${index}`}>Treatment {index}</label>
+          <input
+            className="outline-none border px-2 py-1"
+            type="text"
+            name={`treatment-name-${index}`}
+            id={`treatment-name-${index}`}
+            value={treat}
+          />
+        </div>
+        <button className="w-fit">Remove</button>
+      </li>
     );
   });
+
   return (
-    <div>
-      <div>Tooth Number: {toothNumber}</div>
-      <div>
-        <label htmlFor="description">Description: </label>
-        <input
+    <div className="flex flex-col gap-4 text-xl">
+      <button
+        className="text-3xl self-end"
+        onClick={() => setIsModalOpen(false)}
+      >
+        X
+      </button>
+      <div className="text-3xl">Tooth Number: {toothNumber}</div>
+      <div className="flex gap-4">
+        <label className="" htmlFor="description">
+          Description:{" "}
+        </label>
+        <textarea
+          className="w-full border px-2"
           type="text"
           name="description"
           id="description"
+          rows={4}
           value={toothDescription || ""}
         />
       </div>
-      <div>
-        <h2>Treatments applied to {toothNumber}</h2>
-        {treatments}
+      <div className="flex flex-col gap-4 border rounded p-4 px-2">
+        <h2 className="text-3xl">
+          Treatments applied before
+        </h2>
+        <ul className="flex flex-col gap-2 w-full">{treatments}</ul>
         <button>Add Treatment</button>
       </div>
     </div>
--- client/src/components/PatientInfo.jsx
+++ client/src/components/PatientInfo.jsx
@@ -1,27 +1,28 @@
 import Teeth from "./Teeth.jsx";
-export default function PatientInfo() {
+export default function PatientInfo({
+  name,
+  birthDate,
+  isAdult,
+  hasWisdomTeeth,
+  definedTeeth,
+  saveDate,
+  changeDate,
+}) {
   return (
-    <div>
-      <h1>Ali Kerem Ata</h1>
-      <p>Yaşı: 22</p>
+    <div className="flex flex-col w-1/2 items-start gap-6">
+      <div className="mb-12">
+        <h1 className="text-3xl text-center">{name}</h1>
+        <p className="text-center">Birthdate: {birthDate}</p>
+      </div>
       <Teeth
-        isAdult={true}
-        hasWisdomTeeth={true}
-        definedTeeth={[
-          {
-            toothNumber: 27,
-            description: "Bla Bla",
-            treatmentsBefore: ["Implant", "Whitening"],
-          },
-          {
-            toothNumber: 33,
-            description: "Bla Bla",
-            treatmentsBefore: ["Implant", "Whitening"],
-          },
-        ]}
+        isAdult={isAdult}
+        hasWisdomTeeth={hasWisdomTeeth}
+        definedTeeth={definedTeeth}
       />
-      <i>Hasta Bilgilerinin Kaydedilme Tarihi: 01.01.2023</i>
-      <i>Hasta Bilgilerinin Son Değiştirilme Tarihi: 01.01.2024</i>
+      <div className="text-xs italic self-end">
+        <p>Patient Information Recording Date: {saveDate}</p>
+        <p>Last Modification Date of Patient Information: {changeDate}</p>
+      </div>
     </div>
   );
 }
