e73906d962823384f34fc67bdebeea30f9001db3
Ali Kerem Ata <96057812+sabakukhyraa@users.noreply.github.com>
2024-02-14 15:38:28 +0300

    style & isNew prop for PatientInfo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -30,59 +30,65 @@ function App() {
   return (
     <Router>
       <div className="flex flex-col items-center w-full">
-        <Routes>
-          <Route
-            path="/patient"
-            element={
-              <PatientInfo
-                name={patient?.name}
-                birthDate={patient?.birthDate}
-                isAdult={patient?.isAdult}
-                hasWisdomTeeth={patient?.hasWisdomTeeth}
-                definedTeeth={patient?.definedTeeth}
-                saveDate={patient?.createdAt}
-                changeDate={patient?.updatedAt}
-              />
-            }
-          />
+        <div className="w-full py-8 bg-white rounded shadow-md">
+          <div className="container">
+            <h1 className="text-3xl font-bold">Dentist App</h1>
 
-          <Route
-            path="/"
-            element={
-              <div className="flex flex-col items-center">
-                <div className="flex items-center gap-6 text-2xl">
-                  <label htmlFor="search-key">Search:</label>
-                  <input
-                    className="px-2 py-1 my-5"
-                    type="text"
-                    name="search-key"
-                    id="search-key"
-                    value={searchKey}
-                    onChange={(e) => setSearchKey(e.target.value)}
+          </div>
+        </div>
+        <div className="container">
+          <div className="w-full">
+            <Routes>
+              <Route
+                path="/patient"
+                element={
+                  <PatientInfo
+                    name={patient?.name}
+                    birthDate={patient?.birthDate}
+                    isAdult={patient?.isAdult}
+                    hasWisdomTeeth={patient?.hasWisdomTeeth}
+                    definedTeeth={patient?.definedTeeth}
+                    saveDate={patient?.createdAt}
+                    changeDate={patient?.updatedAt}
                   />
-                </div>
-                <ul className="w-full border-b-2">
-                  {patients &&
-                    filteredPatients.map((p) => (
-                      <PatientList
-                        key={p._id}
-                        patientName={p.name}
-                        patientId={p._id}
-                      />
-                    ))}
-                </ul>
-                <Link
-                  to="/new-patient"
-                  className="w-full p-2 mt-5 text-white rounded-md bg-sky-500"
-                >
-                  Add New Patient
-                </Link>
-              </div>
-            }
-          />
+                }
+              />
 
-          <Route path="/new-patient" element={<PatientInfo />} />
-        </Routes>
+              <Route
+                path="/"
+                element={
+                  <div className="flex flex-col items-center">
+                    <div className="flex items-center gap-6 text-2xl">
+                      <label htmlFor="search-key">Search:</label>
+                      <input
+                        className="px-2 py-1 my-5"
+                        type="text"
+                        name="search-key"
+                        id="search-key"
+                        value={searchKey}
+                        onChange={(e) => setSearchKey(e.target.value)}
+                      />
+                    </div>
+                    <ul className="w-full border-b-2">
+                      {patients &&
+                        filteredPatients.map((p) => (
+                          <PatientList key={p._id} patient={p} />
+                        ))}
+                    </ul>
+                    <Link
+                      to="/new-patient"
+                      className="w-full p-2 mt-5 text-center text-white rounded-md bg-sky-500"
+                    >
+                      Add New Patient
+                    </Link>
+                  </div>
+                }
+              />
+
+              <Route path="/new-patient" element={<PatientInfo isNew={true} />} />
+            </Routes>
+          </div>
+        </div>
       </div>
     </Router>
   );
