12c4876a20a5eaef20576c0ab81c5d840b0c5703
Ali Kerem Ata <96057812+sabakukhyraa@users.noreply.github.com>
2024-02-14 15:37:31 +0300

    tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "992px",
+      xl: "1080px",
+      "2xl": "1294px",
+      "3xl": "1594px",
+    },
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+        },
+      },
+    },
   },
   plugins: [],
 };
