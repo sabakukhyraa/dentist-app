e24d840aadbd57c61535e20f9c1068fc50ee6397
Ali Kerem Ata <96057812+sabakukhyraa@users.noreply.github.com>
2024-02-18 23:09:17 +0300

    doctor and userCredential model created

---
+++ server/models/userCredentialModel.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userCredentialSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  userType: {
+    type: String,
+    enum: ["Doctor", "Patient"],
+    required: true,
+  },
+});
+
+
+module.exports = mongoose.model("UserCredential", userCredentialSchema);
