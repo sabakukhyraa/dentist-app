a00829a53202a4eb3b5dfc12a1aaae99f309ee74
Ali Kerem Ata <96057812+sabakukhyraa@users.noreply.github.com>
2024-02-19 12:39:47 +0300

    user creation and password hash with bcrypt

--- server/models/userCredentialModel.js
+++ server/models/userCredentialModel.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
 const Schema = mongoose.Schema;
 
 const userCredentialSchema = new Schema({
@@ -18,5 +20,23 @@ const userCredentialSchema = new Schema({
   },
 });
 
+// Static signup method
+userCredentialSchema.statics.signup = async (email, password, userType) => {
+  const userExists = await this.findOne({ email });
+  if (userExists) {
+    throw new Error("Email already in use");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const user = this.create({
+    email,
+    password: hashedPassword,
+    userType
+  });
+
+  return user;
+};
 
 module.exports = mongoose.model("User", userCredentialSchema);
