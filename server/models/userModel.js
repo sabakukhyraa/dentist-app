01e4e1f42ded1fb878eb16b0517c5cc85db4d320
Ali Kerem Ata <96057812+sabakukhyraa@users.noreply.github.com>
2024-03-29 00:20:02 +0300

    user model revamped for role property

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -2,8 +2,6 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const bcrypt = require("bcrypt");
 
-const Doctor = require("./models/doctorModel.js");
-
 const userCredentialSchema = new Schema({
   email: {
     type: String,
@@ -14,19 +12,29 @@ const userCredentialSchema = new Schema({
     type: String,
     required: true,
   },
-  userType: {
-    type: Schema.Types.ObjectId,
-    ref: ["Doctor", "Patient"],
+  role: {
+    type: String,
+    enum: ["Doctor", "Patient"],
     required: true,
+  },
+  doctorInfo: {
+    type: Schema.Types.ObjectId,
+    refPath: "role",
+    required: this.role == "Doctor" ? true : false,
+  },
+  patientInfo: {
+    type: Schema.Types.ObjectId,
+    refPath: "role",
+    required: this.role == "Patient" ? true : false,
   },
 });
 
-// Static signup method
-userCredentialSchema.statics.signup = async function (
+// Static signUp method
+userCredentialSchema.statics.signUp = async function (
   email,
   password,
-  userType,
-  name = null
+  role,
+  userInfo,
 ) {
   const userExists = await this.findOne({ email });
   if (userExists) {
@@ -36,25 +44,31 @@ userCredentialSchema.statics.signup = async function (
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
 
-  if (name) {
-    const newDoctor = await Doctor.create({
-      name,
+
+  if (role == "Doctor") {
+    const user = this.create({
+      email,
+      password: hashedPassword,
+      role,
+      doctorInfo: userInfo,
     });
 
+    return user;
+
+  } else if (role == "Patient") {
     const user = this.create({
       email,
       password: hashedPassword,
-      userType: newDoctor,
+      role,
+      patientInfo: role == "Patient" ? userInfo : null,
     });
+
+    return user;
+
   } else {
-    const user = this.create({
-      email,
-      password: hashedPassword,
-      userType,
-    });
+    throw new Error("Invalid role!");
   }
 
-  return user;
 };
 
 module.exports = mongoose.model("User", userCredentialSchema);
