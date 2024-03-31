e41bd9c6e45c52aa01996a20f79ee034be5e05e7
Ali Kerem Ata <96057812+sabakukhyraa@users.noreply.github.com>
2024-03-31 22:45:27 +0300

    "Add login and register links to the header"

--- client/src/components/TheHeader.jsx
+++ client/src/components/TheHeader.jsx
@@ -4,9 +4,22 @@ export default function Header() {
   return (
     <div className="w-full py-8 bg-white rounded shadow-md">
       <div className="container">
-        <h1 className="text-3xl font-bold text-sky-500">
-          <Link to={"/"}>Dentist App</Link>
-        </h1>
+        <div className="flex items-center justify-between w-full">
+          <h1 className="text-3xl font-bold text-sky-500">
+            <Link to={"/"}>Dentist App</Link>
+          </h1>
+          <nav className="space-x-4">
+            <Link className="link-button" to={"/login"}>
+              Login
+            </Link>
+            <Link className="link-button" to={"/sign-up"}>
+              Register
+            </Link>
+            <button className="link-button" type="button">
+              Log out
+            </button>
+          </nav>
+        </div>
       </div>
     </div>
   );
