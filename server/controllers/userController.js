0d80022821c6943c3a5e7c15719f8b69da028158
Ali Kerem Ata <96057812+sabakukhyraa@users.noreply.github.com>
2024-03-31 20:56:05 +0300

    Add jsonwebtoken for user authentication
    * Import jsonwebtoken and create a createToken function in userController.js
    * Add jsonwebtoken to package-lock.json and install its dependencies
    * Add token to response payload for signUpDoctorUser and signUpPatientUser functions in userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,13 @@
-const User = require('../models/userModel.js');
+const User = require("../models/userModel.js");
+const jwt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+  return jwt.sign(
+    { _id }, // payload
+    process.env.SECRET_STRING,
+    { expiresIn: "3d" }
+  );
+};
 
 // login user
 const loginUser = async (req, res) => {
@@ -7,13 +16,13 @@ const loginUser = async (req, res) => {
 
 // sign up doctor user
 const signUpDoctorUser = async (req, res) => {
-
   const { email, password, name } = req.body;
 
   try {
     const user = await User.signUp(email, password, "Doctor", name);
+    const token = createToken(user._id)
 
-    res.status(200).json({ email, user });
+    res.status(200).json({ email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
@@ -21,13 +30,13 @@ const signUpDoctorUser = async (req, res) => {
 
 // sign up patient user
 const signUpPatientUser = async (req, res) => {
-
   const { email, password, patient } = req.body;
 
   try {
     const user = await User.signUp(email, password, "Patient", patient);
+    const token = createToken(user._id);
 
-    res.status(200).json({ email, user });
+    res.status(200).json({ email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
