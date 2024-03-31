745b980860e5f47535b048e4e4e33e8fae2ac707
Ali Kerem Ata <96057812+sabakukhyraa@users.noreply.github.com>
2024-03-31 22:58:06 +0300

    "Add state and form handling to Login and Register components"

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,40 @@
+import { useState } from "react"
+
 export default function Login() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    console.log(email, password)
+  }
+
   return (
-    <div>Login</div>
-  )
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+
+      <button type="submit">Login</button>
+    </form>
+  );
 }
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,52 @@
+import { useState } from "react";
+
 export default function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordAgain, setPasswordAgain] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    if (password !== passwordAgain) return alert('Passwords do not match')
+        
+    console.log(email, password)
+  }
+
   return (
-    <div>Register</div>
-  )
+    <form className="" onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password-again">Password Again:</label>
+        <input
+          type="password"
+          name="password-again"
+          id="password-again"
+          value={passwordAgain}
+          onChange={(e) => setPasswordAgain(e.target.value)}
+        />
+      </div>
+
+      <button type="submit">Register</button>
+    </form>
+  );
 }
