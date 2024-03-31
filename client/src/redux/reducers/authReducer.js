1594eb29aa7612c05fa544222feca9d118deb675
Ali Kerem Ata <96057812+sabakukhyraa@users.noreply.github.com>
2024-03-31 22:03:35 +0300

    authReducer created

---
+++ client/src/redux/reducers/authReducer.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: null,
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.user = action.payload;
+    },
+    logout: (state) => {
+      state.user = null;
+    }
+  }
+});
