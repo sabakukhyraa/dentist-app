c53ac004cf80550e1c489fddec8d2074b7f64d5d
Ali Kerem Ata <96057812+sabakukhyraa@users.noreply.github.com>
2024-04-14 15:59:46 +0300

    getDoctorName controller func created

--- server/controllers/doctorController.js
+++ server/controllers/doctorController.js
@@ -1,5 +1,6 @@
 const Doctor = require("../models/doctorModel.js");
 
+// POST a new doctor
 const createDoctor = async (name) => {
   const newDoctor = await Doctor.create({ name });
 
@@ -8,4 +9,17 @@ const createDoctor = async (name) => {
   return savedDoctor
 }
 
-module.exports = { createDoctor };
+// GET name of the doctor
+const getDoctorName = async (req, res) => {
+  const doctorInfo = req.user.doctorInfo;
+  console.log(doctorInfo);
+
+  try {
+    const name = await Doctor.findById(doctorInfo.doctorInfo).select("name");
+    return res.status(200).json(name);
+  } catch (error) {
+    return res.status(400).json( {error: error.message} )
+  }
+}
+
+module.exports = { createDoctor, getDoctorName };
