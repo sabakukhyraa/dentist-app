a6e2dc015505ffbcaf1592d89af429e0fbfd5547
sabakukhyraa <alikerematas@gmail.com>
2024-05-19 21:34:57 +0300

    DELETE patient route tests

--- server/__tests__/patient.test.js
+++ server/__tests__/patient.test.js
@@ -36,6 +36,9 @@ const testPatientPayload = { //test patient to create or update
   ]
 }
 
+var createdPatientResponse;
+
+const testId = new mongoose.Types.ObjectId().toString()
 const patientToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjFjNTY4NDk1ZDNiZDZmNDgwYTM4MDkiLCJpYXQiOjE3MTYxMjE5MjYsImV4cCI6MTcxNjM4MTEyNn0.Q-RbAljOj5mqHg4PbxPz3rpf5yvxuS3FS_yNHOMpkus" //test user - patient
 const doctorToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjRhMWNiMDM0ZmMxZGJkMzU2NmFjZWYiLCJpYXQiOjE3MTYxMzMwNDAsImV4cCI6MTcxNjM5MjI0MH0.VXqxOIq-uuoUKIA5YZ_BoelmQxJSUocGgthchQj_PFI" //test user - doctor
 
@@ -76,43 +79,66 @@ describe('CREATE patient route and Authorization test', () => {
   describe('Given the user is logged in and authorized.', () => {
     test('Should return a 200 status', async () => {
       
-      const { body, statusCode } = await global.agent
+      createdPatientResponse = await global.agent
         .post('/api/patients')
         .set('Authorization', `Bearer ${doctorToken}`)
         .send(testPatientPayload)
 
-      expect(statusCode).toBe(201);
-      expect(body).toEqual({
-           "__v": 1,
-           "_id": expect.any(String),
-           "birthDate": "1980-01-11T09:00:00.000Z",
-           "createdAt": expect.any(String),
-           "definedTeeth": [
-             {
-               "_id": expect.any(String),
-               "description": "TEST'",
-               "toothNumber": 14,
-               "treatmentsBefore": [
-                 "Implant",
-               ],
-             },
-             {
-               "_id": expect.any(String),
-               "description": "TEST",
-               "toothNumber": 15,
-               "treatmentsBefore": [
-                 "Implant",
-                 "Whitening",
-               ],
-             },
-           ],
-           "doctor": "664a1caf34fc1dbd3566acec",
-           "hasWisdomTeeth": false,
-           "isAdult": true,
-           "name": "TEST",
-           "updatedAt": expect.any(String),
-         })
+      expect(createdPatientResponse.statusCode).toBe(201);
+      expect(createdPatientResponse.body).toEqual({
+        "__v": 1,
+        "_id": expect.any(String),
+        "birthDate": "1980-01-11T09:00:00.000Z",
+        "createdAt": expect.any(String),
+        "definedTeeth": [
+          {
+            "_id": expect.any(String),
+            "description": "TEST'",
+            "toothNumber": 14,
+            "treatmentsBefore": [
+              "Implant",
+            ],
+          },
+          {
+            "_id": expect.any(String),
+            "description": "TEST",
+            "toothNumber": 15,
+            "treatmentsBefore": [
+              "Implant",
+              "Whitening",
+            ],
+          },
+        ],
+        "doctor": "664a1caf34fc1dbd3566acec",
+        "hasWisdomTeeth": false,
+        "isAdult": true,
+        "name": "TEST",
+        "updatedAt": expect.any(String),
+      })
       
     }, 15000)
+  })
+})
+
+describe('DELETE patient route test', () => {
+  describe('Given Patient Id is invalid.', () => {
+    test('Should return a 500 status', async () => {
+      await global.agent.delete('/api/patients/1').set('Authorization', `Bearer ${doctorToken}`).expect(400)
+    }, 15000)
+  })
+  describe('Given Patient Id does not exist.', () => {
+    test('Should return a 404 status', async () => {
+      await global.agent.delete(`/api/patients/${testId}`).set('Authorization', `Bearer ${doctorToken}`).expect(404)
+    }, 15000)
+  })
+  describe('Given Patient Id does exist and patient deleted.', () => {
+    test('Should return a 200 status', async () => {
+      await global.agent.delete(`/api/patients/${createdPatientResponse.body._id}`).set('Authorization', `Bearer ${doctorToken}`).expect(200)
+    }, 15000)
+    describe('Given the Patient Id of deleted patient doesn\'t exist', () => {
+      test('Should return a 404 status', async () => {
+        await global.agent.get(`/api/patients/${createdPatientResponse.body._id}`).set('Authorization', `Bearer ${doctorToken}`).expect(404)
+      }, 15000)
+    })
   })
 })
