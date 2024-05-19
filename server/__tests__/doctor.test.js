8878ba36ff1f807ac6908cab61b42e890ffb64b6
sabakukhyraa <alikerematas@gmail.com>
2024-05-19 22:44:05 +0300

    doctor test

---
+++ server/__tests__/doctor.test.js
@@ -0,0 +1,42 @@
+const supertest = require('supertest');
+const app = require('../app.js')
+const jwt = require("jsonwebtoken");
+const mongoose = require('mongoose')
+
+let server;
+beforeAll(async () => {
+  await mongoose.connect(process.env.DB_URL, {
+    dbName: "DentistApp",
+  });
+  server = app.listen(4000, () => {
+    
+  });
+
+  await new Promise((resolve) => {
+    server.once('listening', resolve);
+  });
+  global.agent = supertest.agent(server);
+}, 15000);
+afterAll(async () => {
+  await mongoose.connection.close();
+  server.close();
+}, 15000);
+
+const testId = new mongoose.Types.ObjectId().toString()
+const patientToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjFjNTY4NDk1ZDNiZDZmNDgwYTM4MDkiLCJpYXQiOjE3MTYxNDQ1MjYsImV4cCI6MTcxNjQwMzcyNn0.CSUG3Qqhou6RhFccK-DUvBGX3rdQxu5_UobGJdsJ9H4" //test user - patient
+const doctorToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjQ5MWQ2Yjg5ZjEyMzI0ODM0ODMxYmYiLCJpYXQiOjE3MTYxNDQ2MDIsImV4cCI6MTcxNjQwMzgwMn0.JQ7K3oGU69JpM1kDLJ0J-a8SmBPEV7cF6whEqmrcx4Y" //test user - doctor
+
+
+describe('GET doctor name', () => {
+  test('Should return a doctor name', async () => {
+    
+  })
+})
+
+describe('COUNT the patients of a doctor', () => {
+  test('Should return a 201 status code and a Number', async () => {
+    const {body, statusCode} = await global.agent.get('/api/patients/count').set('Authorization', `Bearer ${doctorToken}`)
+    expect(statusCode).toBe(201)
+    expect(body).toEqual(expect.any(Number))
+  })
+})
